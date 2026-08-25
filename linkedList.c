0ac3f56dd22703ed5f0a42a73f397d21adcbf71d
Nafis <nafis.mail090@gmail.com>
2026-08-25 15:08:28 +0530

    add createNode function

--- linkedList.c
+++ linkedList.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct node
 {
     int data;
-    struct Node *next;
-};
+    struct node *next;
+} Node;
+
+Node* createNode(int d){
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = d;
+    newNode->next = NULL;
+    return newNode;
+}
 
 int main() {
-    
+    Node *head = createNode(10);
+    Node *second = createNode(20);
+    Node *third = createNode(30);
     return 0;
 }
