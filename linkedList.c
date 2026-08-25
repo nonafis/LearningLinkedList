32696a40fbe10482bafd50e7c01f56000c2e836f
Nafis <nafis.mail090@gmail.com>
2026-08-25 15:17:27 +0530

    finalize appendNode over append and rename insertNode to spliceNode, add comments, demonstrate each function in use

--- linkedList.c
+++ linkedList.c
@@ -7,39 +7,21 @@ typedef struct node
     struct node *next;
 } Node;
 
-Node *createNode(int d)
+Node *createNode(int d) // to avoid code duplication
 {
-    Node *newNode = malloc(sizeof(Node));
-    if (newNode == NULL)
+    Node *newNode = malloc(sizeof(Node)); // allocation
+    if (newNode == NULL)                  // checks if allocation so that null pointer does not get dereferenced later on
     {
         printf("Node creation failed. Memory Allocation Unsuccessful.\n");
         exit(1);
     }
-    else
+    else // initializes the Node
     {
         newNode->data = d;
         newNode->next = NULL;
         return newNode;
     }
-}
-
-void append(Node **headadd, int d)
-{
-    Node *newNode = createNode(d);
-    if (*headadd == NULL)
-    {
-        *headadd = newNode;
-    }
-    else
-    {
-        Node *current = *headadd;
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = newNode;
-    }
-}
+} // reusable function
 
 void printNode(Node *head)
 {
@@ -50,43 +32,43 @@ void printNode(Node *head)
     }
     else
     {
-        while (current->next != NULL)
+        while (current->next != NULL) // traverse and prints but does not prints the last node since at last node current->next==NULL hence the loop wont execute to print the last node
         {
             printf("%d->", current->data);
             current = current->next;
         }
-        printf("%d->NULL\n", current->data);
+        printf("%d->NULL\n", current->data); // prints the last node
     }
 }
 
-void insertNode(Node **headadd, Node *N)
+void spliceNode(Node **headadd, Node *N) //adds a pre created node to the end of a list
 {
-    if (*headadd == NULL)
+    if (N->next == NULL)
     {
-        *headadd = N;
+        if (*headadd == NULL)
+        {
+            *headadd = N;
+        }
+        else
+        {
+            Node *current = *headadd;
+            while (current->next != NULL)
+            {
+                current = current->next;
+            }
+            current->next = N;
+        }
     }
     else
     {
-        Node *current = *headadd;
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = N;
+        printf("Invalid argument! it is not a single node.\n");
     }
 }
 
 void appendNode(Node **headadd, int d)
 {
     Node *newNode = createNode(d);
-    insertNode(headadd, newNode);
-}
-
-void insertAtHead(Node **headadd, int d)
-{
-    Node *newNode = createNode(d);
-    insertNode(&newNode, *headadd);
-    *headadd = newNode;
+    spliceNode(headadd, newNode);
 }
 
 void insert_at_head(Node **headadd, int d)
@@ -99,48 +81,37 @@ void insert_at_head(Node **headadd, int d)
 void insert_at_pos(Node **headadd, int d, int pos)
 {
     Node *newNode = createNode(d);
-    if (*headadd == NULL)
+    if (*headadd == NULL || pos == 0)
     {
+        newNode->next = *headadd;
         *headadd = newNode;
     }
     else
     {
         Node *current = *headadd;
-        Node *nextToCurrent = *headadd;
-        if (pos == 0)
+        for (int i = 1; (current->next != NULL) && (i < pos); i++)
         {
-            newNode->next = nextToCurrent;
-            *headadd = newNode;
+            current = current->next;
         }
-        else
-        {
-            for (int i = 0; (nextToCurrent != NULL) && (i < pos); i++)
-            {
-                nextToCurrent = nextToCurrent->next;
-                if (i != 0)
-                {
-                    current = current->next;
-                }
-            }
-            newNode->next = nextToCurrent;
-            current->next = newNode;
-        }
+        newNode->next = current->next;
+        current->next = newNode;
     }
 }
 
-void insert_after_key(Node **headadd, int d, int key){
-    if (*headadd==NULL)
+void insert_after_key(Node **headadd, int d, int key)
+{
+    if (*headadd == NULL)
     {
         printf("Key not found!\n");
     }
     else
     {
         Node *current = *headadd;
-        while ((current->next!=NULL)&&((current->data)!=key))
+        while ((current->next != NULL) && ((current->data) != key))
         {
-            current=current->next;
+            current = current->next;
         }
-        if ((current->next==NULL)&&(current->data!=key))
+        if ((current->next == NULL) && (current->data != key))
         {
             printf("Key not found!\n");
         }
@@ -170,25 +141,43 @@ int main()
 {
     Node *head = NULL;
 
-    append(&head, 10);
-    append(&head, 20);
-    append(&head, 30);
-    append(&head, 40);
-    append(&head, 50);
+    appendNode(&head, 10);
+    appendNode(&head, 20);
+    appendNode(&head, 30);
+    appendNode(&head, 40);
+    appendNode(&head, 50);
+    printNode(head);
+
+    insert_at_head(&head, 35);
+    printNode(head);
+
+    insert_at_head(&head, 90);
+    printNode(head);
+
+    insert_at_pos(&head, 70, 5);
+    printNode(head);
+
+    insert_at_pos(&head, 88, 8);
+    printNode(head);
+
+    insert_at_pos(&head, 65, 0);
+    printNode(head);
+
+    insert_at_pos(&head, 43, 15);
     printNode(head);
 
     insert_after_key(&head, 37, 10);
     printNode(head);
-    
+
     insert_after_key(&head, 86, 37);
     printNode(head);
 
-    insert_after_key(&head, 45, 50);
+    insert_after_key(&head, 45, 43);
     printNode(head);
 
     insert_after_key(&head, 23, 89);
     printNode(head);
-    
+
     freeList(&head);
     return 0;
 }
